27b89646698c4ea506c594d006a65c4a2763c65b
kushal Agarwal <kushalag02@gmail.com>
2024-03-14 15:35:51 +0530

    Added routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="page-content">
+            <div>Home</div>
+        </div>
+
+
+    )
+}
+
+export default Home
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login = () => {
+    return (
+        <div className="page-content">
+            <div>Login here</div>
+        </div>
+    )
+}
+
+export default login
