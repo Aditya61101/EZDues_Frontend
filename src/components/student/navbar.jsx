492694622079573bad026bb4604c820385e61eb2
Aditya Kumar <adityakumarsanni.2001@gmail.com>
2024-04-10 01:10:52 +0530

    changes on student pages

--- src/components/student/navbar.jsx
+++ src/components/student/navbar.jsx
@@ -1,25 +1,21 @@
-import React from "react";
 import "./navbar.css";
-import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import { images } from "@/constants";
 
 const StudentNavbar = () => {
   return (
-    <>
-      {/* <div className="navbar">
-        <div className="navbar-heading">EZDues</div>
-        <div className="circle"></div>
-      </div> */}
-      <Navbar bg="light" expand="lg" className="nav justify-content-between">
-        <h1 className="navbar-heading">EZDues</h1>
-        <div className="circle">
-          <img
-            className="ellipse"
-            src="src\assets\user-logo.svg"
-            alt="User Logo"
-          />
-        </div>
-      </Navbar>
-    </>
+    <nav className="flex justify-between sticky my-8">
+      <Link to={"/"} className="flex-shrink-0 flex items-center">
+        <img loading="lazy" src={images.logo} alt="logo" className="h-6" />
+      </Link>
+      <Link to={"/student/profile"} className="circle">
+        <img
+          loading="lazy"
+          src={images.Profile}
+          alt="User Logo"
+        />
+      </Link>
+    </nav>
   );
 };
 
